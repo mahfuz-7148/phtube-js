4e01ba8846703bc5f6b6ac223dd2960ea5968a50
shanto <mrahman7148@gmail.com>
2026-02-07 22:25:44 +0600

    feat: add loadCategoryVideos and displayVideos

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const api_base = 'https://openapi.programming-hero.com/api/phero-tube'
 
+let currentVideos = []
+const displayVideos = (categoryVideos) => {
+ const videoContainer = document.getElementById('video-container')
+  videoContainer.innerHTML = ''
+  currentVideos = categoryVideos || []
+  // console.log(currentVideos)
+}
+const loadCategoryVideos = async (categoryId) => {
+  try {
+    const response = await fetch(`${api_base}/category/${categoryId}`)
+    const data = await response.json()
+    const {category} = data
+    displayVideos(category)
+    // console.log(category)
+  }
+  catch (err) {
+    console.error('error loading categories', err)
+  }
+}
+
 const displayCategories = (categories = []) => {
  const categoryContainer = document.getElementById('category-container')
  categories.forEach(category => {
@@ -7,6 +27,7 @@ const displayCategories = (categories = []) => {
    button.id = `btn-${category.category_id}`
    button.className = "px-6 py-2.5 bg-[#151520] border border-[#2a2a3e] text-gray-300 font-medium rounded-full whitespace-nowrap hover:border-[#ff1f3d] hover:text-white hover:-translate-y-0.5 transition-all duration-300";
    button.textContent = category.category
+   button.onclick = () => loadCategoryVideos(category.category_id)
    categoryContainer.appendChild(button)
    // console.log(button)
  })
